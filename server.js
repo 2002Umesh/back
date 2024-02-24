853a8f2c4f8d7768b2e2acfacf939bdc0085065c
Umesh Das <dasumesh2002@gmail.com>
2024-02-24 14:44:44 +0530

    final again

--- server.js
+++ server.js
@@ -7,15 +7,12 @@ const connectDb = require("./utils/db");
 const authRoute = require("./router/auth-router");
 const contactRoute = require("./router/contact-router");
 const errorMiddleware = require("./middlewares/error-middleware.js");
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT || 5000;
 
 //middleware
 //handling cors policy and hosting
 const corsOptions = {
-  origin: 
- 
-    "https://cloneplusdisney.netlify.app/"
-  ,
+  origin: "https://cloneplusdisney.netlify.app",
   methods: "GET,POST,PUT,DELETE,PATCH,HEAD",
   Credentials: true,
 };
